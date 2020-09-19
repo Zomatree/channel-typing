0ab5eb63ffd61999f821df5a067b99c571e4668d
Zomatree <39768508+Zomatree@users.noreply.github.com>
2020-09-19 18:08:33 +0100

    removed check for muted channel

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ module.exports = class ChannelTyping extends Plugin {
 
     inject('channeltyping-channel', TextChannel.prototype, 'renderIcons', function (_, res) {
       // Other plugins cause this to rerender, leading to duplicated elements.
-      if (!this.props.selected && !this.props.muted && !res.props.children.find(c => c && c.type === TypingIndicator)) {
+      if (!this.props.selected && !res.props.children.find(c => c && c.type === TypingIndicator)) {
         res.props.children.push(React.createElement(TypingIndicator, { channel: this.props.channel }));
       }
       return res;
